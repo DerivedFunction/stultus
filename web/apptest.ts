59ecc359a930ef9fced085c0c353d1cf096e30a2
Patrick Boles <pjb325@lehigh.edu>
2024-03-20 11:28:01 -0400

    added functions for edit tests that initialize and reset conditions

--- web/apptest.ts
+++ web/apptest.ts
@@ -18,13 +18,53 @@ let originalMsg: any;
 
 describe("Edit Tests", function () {
 
+  //post/idea values
+  let msg_string = "Testing msg";
+  let title_string = "Testing title";
+
+  // Our HTML elements
+  let editTitle: any;
+  let editMessage: any;
+  let editButton: any;
+  let editElements: any;
+  let showElements: any;
+  let editCancel: any;
+
+  //function to initialize values 
   beforeAll(function (done: () => void) {
-  
+    editTitle = <HTMLInputElement>document.getElementById("editTitle");
+    editMessage = <HTMLInputElement>document.getElementById("editMessage");
+    editButton = <HTMLButtonElement>document.getElementById("editButton");
+    showElements = document.getElementById("showElements");
+    editElements = document.getElementById("editElement");
+    editCancel = document.getElementById("editCancel");
+    getOriginal();
+    wait(done);
   });
 
+  //function to run before each test to allow backend to sync
   beforeEach(function (done: () => void) {
-    
+    clickEditBtn();
+    wait(done);
   });
+  
+  // function to execute after all the tests are complete that resets the system
+  afterAll(function (done: () => void) {
+    clickEditBtn();
+    // Replace our edited elements with the previous value
+    setTimeout(function () {
+      editMessage.value = originalMsg;
+      editTitle.value = originalTitle;
+      editButton.click();
+      done();
+    }, delay);
+  });
+
+  // function  sure to wait after executing each test
+  afterEach(function (done: () => void) {
+    wait(done);
+  });
+
 
   it("UI Test: Add button hides lists", function () {
     console.log("html function");
@@ -70,3 +110,32 @@ describe("Add Button Tests", function () {
 describe("Like Button Tests", function () {
 
 });
+
+
+// Get original content from first element
+function getOriginal() {
+  setTimeout(function () {
+    originalTitle = document.getElementById("postTitle")?.textContent;
+    originalMsg = document.getElementById("postBody")?.textContent;
+  }, delay);
+}
+
+
+// Add a small delay between each Jasmine test.
+function wait(done: () => void) {
+  setTimeout(function () {
+    done();
+  }, delay);
+}
+
+
+/**
+ *  Click the edit button for the most recent element
+ *  After this, we will need to wait for backend
+ *  to send data over to front end.
+ */
+function clickEditBtn() {
+  let ebtns = document.getElementsByClassName("editbtn");
+  let ebtn = <HTMLButtonElement>ebtns[0];
+  ebtn.click();
+}
