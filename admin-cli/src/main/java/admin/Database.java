62414ff60185b04c49439383caffdf885ff4c3f4
thc225 <thc225@lehigh.edu>
2024-03-19 20:21:33 -0400

    changes to dropTable reflected, no major change in code

--- admin-cli/src/main/java/admin/Database.java
+++ admin-cli/src/main/java/admin/Database.java
@@ -1,11 +1,11 @@
 package admin;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 
 public class Database {
   /**
