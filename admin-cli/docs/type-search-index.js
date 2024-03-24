17677bc99ebc4f6ac9ce5b1505789d1156bb2c9b
thc225 <thc225@lehigh.edu>
2024-03-24 18:21:11 -0400

    Javadoc output

---
+++ admin-cli/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"admin","l":"App"},{"p":"admin","l":"Database"},{"p":"admin","l":"Database.RowData"}];updateSearchResults();
