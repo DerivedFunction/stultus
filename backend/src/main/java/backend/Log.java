d866eb87f84bdde5e320577f97a2811286a81d4a
del226 <del226@lehigh.edu>
2024-03-31 18:46:42 -0400

    `Log` class to log/print messages

---
+++ backend/src/main/java/backend/Log.java
@@ -0,0 +1,40 @@
+package backend;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Simple way to log messages
+ */
+public class Log {
+
+    private static final Logger logger = Logger.getLogger("MyLogger");
+
+    /**
+     * Logs an informational message.
+     *
+     * @param message The message to log.
+     */
+    public static void info(String message) {
+        logger.log(Level.INFO, message);
+    }
+
+    /**
+     * Logs a warning message.
+     *
+     * @param message The message to log.
+     */
+    public static void warning(String message) {
+        logger.log(Level.WARNING, message);
+    }
+
+    /**
+     * Logs an error message.
+     *
+     * @param message The message to log.
+     */
+    public static void error(String message) {
+        logger.log(Level.SEVERE, message);
+    }
+
+}
